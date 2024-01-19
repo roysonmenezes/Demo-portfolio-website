f9664d53bbd874a66eb7b458d2e63f53f3f6c97a
roysonmenezes <royson32004@gmail.com>
2024-01-19 21:32:25 +0530

    edited css and html file

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ var typed=new Typed(".multiple-text",{
     backDelay:100,
     loop:true
 });
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top
+    })
+};
